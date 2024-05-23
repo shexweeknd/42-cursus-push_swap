b85d8297093e696513ceaecd7ecae34e7599e22a
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r10s2.42antananarivo.mg>
2024-05-23 07:34:26 +0300

    effectue:     - ajout des commentaires sur la recuperation des argv todo:     - optimiser l'algo de capture des argv

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,28 @@
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 07:38:14 by hramaros          #+#    #+#             */
-/*   Updated: 2024/05/23 07:15:31 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/05/23 07:33:29 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(void)
+int	verify_argv(int argc, char **argv)
 {
+	// voir si un element de argv est !ft_isdigit alors return 1
+	// creer un array de taille argc pour contenir les atoi
+	// faire un ft_atoi de chaque composant de argv puis remplir dans le array
+	// verifier les repetitions dans l'array puis return 1 si erreur
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (!(argc >= 2) || !verify_argv(argv))
+	{
+		write(1, "Error\n", 6);
+		return (1);
+	}
+	// split des argv
 	return (0);
 }
